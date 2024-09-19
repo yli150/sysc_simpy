4cea9a8671bec021bcea818aa43905776fa00bdf
yli150 <victor.y.li@intel.com>
2024-09-19 14:54:05 +0800

    init commit

---
+++ examples/01_thread/module_with_thread.py
@@ -0,0 +1,18 @@
+from syscsim.sc_module import SCModule
+import simpy 
+
+class ModuleB(SCModule):
+    def __init__(self, env, name) -> None:
+        super().__init__(env, name)
+        self.env.process(self.thread())
+
+    def thread(self):
+        while True:
+            print(f'Run @{self.env.now}')
+            yield self.env.timeout(1)
+
+
+if __name__ == '__main__':
+    env = simpy.Environment()
+    m = ModuleB(env, 'b')
+    env.run(10)
---
+++ syscsim/sc_clock.py
@@ -0,0 +1,20 @@
+from simpy import Environment
+import simpy 
+
+
+class Clock():
+    '''
+    Clock is a particular event which runs in one cycle 
+    '''
+    def __init__(self, env: Environment, name: str) -> None:
+        self.env = env
+        self.name = name
+        self.env.process(self.run())
+
+    def run(self):
+        while True:
+            self.event = self.env.timeout(1)
+            yield self.event 
+
+    def wait(self):
+        return self.event 
